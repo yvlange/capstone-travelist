6957fced711472d73b3c29df385cf8ac2405cc4e
Yvonne Lange <yvonne-lange@web.de>
2021-04-26 12:36:07 +0200

    add component folder and add structure to the navbar in the footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Add from "./components/Add";
+import Trips from "./components/Trips";
+import Profile from "./components/Profile";
 
 function App() {
-  return <div className="App">travelist.</div>;
+  return (
+    <Router>
+      <div className="App">
+        <header>travelist.</header>
+        <main className="Main"></main>
+        <footer className="Footer">
+          <Switch>
+            <Route path="/add">
+              <Add />
+            </Route>
+            <Route path="/trips">
+              <Trips />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </footer>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
