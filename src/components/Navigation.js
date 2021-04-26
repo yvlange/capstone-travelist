096d78dde13286e37e4997db71657460258d8bbf
Yvonne Lange <yvonne-lange@web.de>
2021-04-26 17:20:32 +0200

    Add icons to the footer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,20 @@
 import "./Navigation.css";
-
+import { FiPlus, FiUser } from "react-icons/fi";
+import { FaSuitcaseRolling } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
     <nav className="navigation">
-      <NavLink to="/add">Add</NavLink>
-      <NavLink to="/trips">Save Trips</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
+      <NavLink to="/add">
+        <FiPlus className="navigation__plus" />
+      </NavLink>
+      <NavLink to="/trips">
+        <FaSuitcaseRolling className="navigation__suitcase" />
+      </NavLink>
+      <NavLink to="/profile">
+        <FiUser className="navigation__user" />
+      </NavLink>
     </nav>
   );
 }
