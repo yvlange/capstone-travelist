893de98cb6884050f9666a1c0e1d1afec148adc0
Yvonne Lange <yvonne-lange@web.de>
2021-04-27 11:42:05 +0200

    Add AddForm component and install headlessui package

---
+++ src/components/AddForm.js
@@ -0,0 +1,10 @@
+function AddForm() {
+  return (
+    <form className="form">
+      <div className="form__destination">
+        <input type="text" placeholder="enter destination" />
+      </div>
+    </form>
+  );
+}
+export default AddForm;
