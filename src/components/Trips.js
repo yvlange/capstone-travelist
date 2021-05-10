38438dc42336a844a4eafb7f31216750c7129570
Yvonne Lange <yvonne-lange@web.de>
2021-05-10 07:09:42 +0200

    Receive random picture from unsplash depending on destination

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -6,16 +6,31 @@ import NoTripsPlaceholder from "./NoTripsPlaceholder";
 
 function Trips() {
   const [trips, setTrips] = useState([]);
+  const [randomImage, setRandomImage] = useState();
 
   useEffect(() => {
     const trip = getTripsFromLocalStorage("tripData");
     setTrips(trip);
-  }, []);
+    const destination = trip[0].destination;
+    console.log(destination);
+
+    const url = `https://api.unsplash.com/search/photos?page=1&query=${destination}&client_id=6ybgaYshLtwpaIZkxx94OkGlOYYUO-8C6eOegB8jHFY`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const photo = data.results[0].urls.small;
+        setRandomImage(photo);
+        console.log(photo);
+      });
+  }, [trips.destination]);
+  console.log(trips.destination);
 
   function renderTrips() {
     return trips.map((trips, index) => {
       return (
         <div>
+          <img src={randomImage} alt="destination" />
+
           <Link to={`/saved-trip/${trips.id}`}>
             <p key={index}>{trips.destination}</p>
           </Link>
