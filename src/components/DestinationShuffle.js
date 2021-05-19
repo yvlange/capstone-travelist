3d7804b031448852ef1a846c4a4281c79ce9d42a
Yvonne Lange <yvonne-lange@web.de>
2021-05-19 08:17:33 +0200

    Uninstall baffle package

--- src/components/DestinationShuffle.js
+++ src/components/DestinationShuffle.js
@@ -3,7 +3,7 @@ import "../styles/DestinationShuffle.css";
 
 function DestinationShuffle() {
   const [capital, setCapital] = useState("");
-  const [flag, setFlag] = useState();
+
   useEffect(() => {
     const url = "https://random-data-api.com/api/nation/random_nation";
 
@@ -12,30 +12,19 @@ function DestinationShuffle() {
 
       .then((data) => {
         const capitalData = data.capital;
-        const flagData = data.flag;
+
         setCapital(capitalData);
-        setFlag(flagData);
       })
       .catch((error) => {
         console.log("Error status: ", error.toString());
       });
   }, []);
 
-  const text = baffle(capital);
-  text.set({
-    characters: "0909dudedggsedcrfefds",
-    speed: 100,
-  });
-
-  text.start();
-  text.reveal(10000);
-
   return (
     <div>
       <h3>need some travel insperation?</h3>
       <section className="inspoContainer">
         <p className="inspoCapital">{capital}</p>
-        <p className="inspoFlag">{flag}</p>
       </section>
     </div>
   );
