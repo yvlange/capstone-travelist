591ca0a944bcded146bf00a479802aa999dbf856
Yvonne Lange <yvonne-lange@web.de>
2021-05-20 14:35:47 +0200

    Destructure the images props

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,27 +3,26 @@ import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 import { useState } from "react";
 import { Image } from "cloudinary-react";
 
-function Carousel(images) {
+function Carousel({ images }) {
   const [photo, setPhoto] = useState(0);
-  //   const imagesArray = images.images;
-  //   const length = imagesArray.length;
+  const length = images?.length;
 
   function nextSlide() {
-    setPhoto(photo === images.images.length - 1 ? 0 : photo + 1);
+    setPhoto(photo === length - 1 ? 0 : photo + 1);
   }
 
   function prevSlide() {
-    setPhoto(photo === 0 ? images.images.length - 1 : photo - 1);
+    setPhoto(photo === 0 ? length - 1 : photo - 1);
   }
 
-  if (!Array.isArray(images.images) || images.images.length <= 0) {
+  if (!Array.isArray(images) || length <= 0) {
     return null;
   }
   return (
     <div className="carousel">
       <BiChevronLeft className="left-arrow" onClick={prevSlide} />
       <BiChevronRight className="right-arrow" onClick={nextSlide} />
-      {images.images.map((image, index) => {
+      {images.map((image, index) => {
         return (
           <div
             className={index === photo ? "slideActive" : "slide"}
